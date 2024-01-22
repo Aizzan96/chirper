fc7c94ccac1713381fb1b8ac1fb320149c28f204
Aizzan96 <aizzan77@gmail.com>
2024-01-22 09:28:03 +0800

    display chirp

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,28 +1,40 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { useForm, Head } from '@inertiajs/vue3';
- 
-const form = useForm({
-    message: '',
-});
+    import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+    import Chirp from "@/Components/Chirp.vue"; //import the Chirp.vue to display
+    import InputError from "@/Components/InputError.vue";
+    import PrimaryButton from "@/Components/PrimaryButton.vue";
+    import {
+        useForm,
+        Head
+    } from "@inertiajs/vue3";
+
+    defineProps(["chirps"]); //define chirps because we use this in controller
+
+    const form = useForm({
+        message: "",
+    });
 </script>
- 
+
 <template>
+
     <Head title="Chirps" />
- 
+
     <AuthenticatedLayout>
-        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-            <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
-                <textarea
-                    v-model="form.message"
-                    placeholder="What's on your mind?"
-                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                ></textarea>
+        <div class="mx-auto max-w-2xl p-4 sm:p-6 lg:p-8">
+            <form
+                @submit.prevent="
+                    form.post(route('chirps.store'), {
+                        onSuccess: () => form.reset(),
+                    })
+                ">
+                <textarea v-model="form.message" placeholder="What's on your mind?"
+                    class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></textarea>
                 <InputError :message="form.errors.message" class="mt-2" />
                 <PrimaryButton class="mt-4">Chirp</PrimaryButton>
             </form>
+            <div class="mt-6 divide-y rounded-lg bg-white shadow-sm">
+                <Chirp v-for="chirp in chirps" :key="chirp.id" :chirp="chirp" />
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
